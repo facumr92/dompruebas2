9dfc5850c7677ab526cb880d4001aa72cf722ab2
Facundo Rubil <facundorubil@MacBook-Air-de-Facundo.local>
2025-06-27 16:54:14 -0300

    modificado2

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const nombre = document.getElementById("nombre");
 const apellido = document.getElementById("apellido");
 const edad = document.getElementById("edad");
 const enviar = document.getElementById("enviar");
-
+const formulario = document.getElementById("formulario"); // Asegúrate de tener este div en tu HTML
 
 enviar.addEventListener("click", (e) => {
-    const resultado = document.getElementById("resultado");
-    //evito que el formulario se envie por defecto para evaluarlo.
     e.preventDefault();
 
     const nombreValor = nombre.value;
@@ -19,57 +17,19 @@ enviar.addEventListener("click", (e) => {
     }
 
     setTimeout(() => {
-        const parrafo = document.createElement  ("p");
-        parrafo.textContent = `Hola ${nombreValor} ${apellidoValor} tuedad es: ${edadValor}`;
-        formulario.innerHTML = "";
-        formulario.appendChild(parrafo);
-
-        nombre.value = "";
-        apellido.value = "";
-        edad.value = "";
+        crearUI(nombreValor, apellidoValor, edadValor);
     }, 3000);
 });
 
-
-
-/*
-boton.addEventListener("click", hacer_algo);
-
-function hacer_algo() {
-    const edad = Number(prompt("¿Qué edad tienes?"));
-
-    // Validar si se ingresó un número
-    if (isNaN(edad) || edad === 0) {
-        alert("Edad no válida.");
-        return;
-    }
-
-    const resultado = edad > 18 ? "Podés votar" : "No podés votar";
-
-    // Limpiar contenido anterior del div ingreso
-    ingreso.innerHTML = "";
-
+function crearUI(nombreValor, apellidoValor, edadValor) {
     const parrafo = document.createElement("p");
-    parrafo.textContent = resultado;
-    ingreso.appendChild(parrafo);
+    parrafo.textContent = `Hola ${nombreValor} ${apellidoValor}, tu edad es: ${edadValor}`;
+    
+    formulario.innerHTML = ""; 
+    formulario.appendChild(parrafo);
+
+    nombre.value = "";
+    apellido.value = "";
+    edad.value = "";
 }
 
-const estilizar = (elemento) => {
-    const estilo = elemento.style;
-
-    estilo.backgroundColor = "crimson";
-    estilo.color = "antiquewhite";
-    estilo.fontSize = "20px";
-    estilo.fontFamily = "Arial";
-    estilo.padding = ".5rem";
-    estilo.marginBottom = "1rem";
-};
-
-estilizar(titulo);
-
-// function generar_random() {
-//     const numR = Math.floor(Math.random() * 5);
-//     return numR;
-// }
-
-*/
