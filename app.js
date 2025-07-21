7a031714fd60d3315b5e6d97ba0e0ad424025a56
Facundo Rubil <facundorubil@MacBook-Air-de-Facundo.local>
2025-07-21 18:06:56 -0300

    subo validaciones

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
-const nombre = document.getElementById("nombre");
-const apellido = document.getElementById("apellido");
-const edad = document.getElementById("edad");
-const enviar = document.getElementById("enviar");
-const formulario = document.getElementById("formulario"); // Asegúrate de tener este div en tu HTML
+document.getElementById("formulario").addEventListener("submit", function(event) {
+   
 
-enviar.addEventListener("click", (e) => {
-    e.preventDefault();
+    //podria reorden el codigo y separarlo en funciones...
+   // Evita que el formulario se envíe
+    event.preventDefault(); 
+  
 
-    const nombreValor = nombre.value;
-    const apellidoValor = apellido.value;
-    const edadValor = edad.value;
+    //guardo los datos en variables
+    const nombre = document.getElementById("nombre").value.trim();
+    const pass = document.getElementById("contrasena").value;
+    const repetir = document.getElementById("repetir").value;
+    const mensaje = document.getElementById("mensaje");
+  
 
-    if (nombreValor === "" || apellidoValor === "" || edadValor === "") {
-        alert("Por favor, completa todos los campos.");
-        return;
+
+    //realizo algunas validaciones
+    if (nombre.length < 3) {
+      mensaje.textContent = "El nombre debe tener al menos 3 caracteres.";
+      return;
     }
+  
+    if (pass.length < 6) {
+      mensaje.textContent = "La contraseña debe tener al menos 6 caracteres.";
+      return;
+    }
+  
+    if (pass !== repetir) {
+      mensaje.textContent = "Las contraseñas no coinciden.";
+      return;
+    }
+  
+    mensaje.style.color = "green";
+    mensaje.textContent = "Formulario válido. Enviando...";
 
-    setTimeout(() => {
-        crearUI(nombreValor, apellidoValor, edadValor);
-    }, 3000);
-});
 
-function crearUI(nombreValor, apellidoValor, edadValor) {
-    const parrafo = document.createElement("p");
-    parrafo.textContent = `Hola ${nombreValor} ${apellidoValor}, tu edad es: ${edadValor}`;
-    
-    formulario.innerHTML = ""; 
-    formulario.appendChild(parrafo);
-
-    nombre.value = "";
-    apellido.value = "";
-    edad.value = "";
-}
-
+    // si está todo bien se envia el formulario
+    // this.submit();
+  });
+  
